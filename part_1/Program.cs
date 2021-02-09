d6769fb9e38db5cec30c26fabcd2e28f18e34bd3
Miksanatik <sapanovich.aleksey@gmail.com>
2021-02-09 16:43:10 +0300

    third lab

---
+++ part_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number in range -5 <= x <= 9\nX = ");
+            double number = 0;
+            double result = 0;
+            if(Double.TryParse(Console.ReadLine(), out number))
+            {
+                if (number >= -5 && number <= 9)
+                {
+
+                    if (number >= -5 && number < -3)
+                    {
+                        result = (number + 3.0) * Math.Tan(Math.PI / 4);
+                    }
+                    else if (number >= -3 && number < 0)
+                    {
+                        result = Math.Sqrt(9 - Math.Pow(number, 2));
+                    }
+                    else if (number >= 0 && number < 6)
+                    {
+                        result = -0.5 * number + 3;
+                    }
+                    else if (number >= 6 && number <= 9)
+                    {
+                        result = (number - 6.0) * Math.Tan(Math.PI / 4);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect number. Correct number is >= -5 and <= 9");
+                    return;
+                }
+                Console.WriteLine("F(x) = {0}", result);
+            }
+        }
+    }
+}
---
+++ part_2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //inital values
+            double x1 = 1;
+            double x2 = 4;
+            double step = 0.15;
+            double epsilon = 0.001;
+
+            //output exp which calculated with iteration method with 
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine(" argument |       result    | steps |  Math.Exp()  ");
+            Console.WriteLine("----------------------------------------------------");
+
+            int quantity = (int)((x2 - x1) / step);
+            for(int i = 0; i <= quantity; i++)
+            {
+                double result = 0;
+                int steps = 0;
+                calculate(x1, epsilon, out steps, out result);
+                Console.WriteLine(" {0:0.00000}  |  {1:0.000000000000} | {2:00}    | {3:0.000000000000} ", x1, result, steps, Math.Exp((-1 * Math.Pow(x1, 2))));
+                x1 += step;
+            }
+
+
+            
+            
+        }
+        public static void calculate(double number, double accuracy, out int steps,out double result)
+        {
+            double nextElement = 1.0;
+            result = nextElement;
+            steps = 0;
+
+            for (; Math.Abs(nextElement) > accuracy; steps++)
+            {
+                nextElement *= number * number;
+                nextElement /= (steps+1);
+                result += nextElement;
+            }
+            result = 1 / result;
+        }
+    }
+}
